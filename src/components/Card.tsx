c1d39169334e8afb36ec2c9f7faa2fd0ee887573
oyelaw <okiki.oyelaw@gmail.com>
2021-11-12 10:40:05 +0100

    setup query to fetch pokemon details in card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,45 @@
 import React from "react";
+import Image from "next/image";
+import { useQuery } from "react-query";
 
 type CardProp = {
   pokemon: { url: string; name: string };
 };
 
+const fetchPokemonObject = async (url: string) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+};
+
+function useFetchPokemon(pokemon: { url: string }) {
+  return useQuery(
+    ["pokemon", pokemon.url],
+    async ({ queryKey }) => {
+      return await fetchPokemonObject(queryKey[1]);
+    },
+    {
+      staleTime: Infinity,
+    }
+  );
+}
+
 const Card = ({ pokemon }: CardProp) => {
+  const { data, status } = useFetchPokemon(pokemon);
   return (
     <div className="card">
-      <p>Pokemon Image</p>
+      {status === "loading" ? (
+        <h2>Loading....</h2>
+      ) : (
+        <Image
+          alt="Picture of the pokemoon"
+          src={data.sprites.front_default}
+          width={200}
+          height={200}
+        />
+      )}
       <p>{pokemon.name}</p>
     </div>
   );
